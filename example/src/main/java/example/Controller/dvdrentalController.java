a5d5852a0c38e629c2b455b72c91321948091270
shiv97 <35682403+shiv97@users.noreply.github.com>
2019-08-18 18:58:05 +0530

    Add files via upload

--- example/src/main/java/example/Controller/dvdrentalController.java
+++ example/src/main/java/example/Controller/dvdrentalController.java
@@ -3,14 +3,18 @@ package example.Controller;
 
 import example.dvdrentalservice.dvdrentalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/aps")
-public class dvdrentalController {
+public class dvdrentalController
+{
 
     @Autowired
     dvdrentalService DvdrentalService;
@@ -32,6 +36,27 @@ public class dvdrentalController {
         }
 
     }
+    @PostMapping("/getdata")
+            public List<Map<String,Object>> submitdata()
+    {
+        List<Map<String, Object>> dat;
+        dat = DvdrentalService.getdata();
+        System.out.println(dat);
+        return dat;
+    }
+    /*@PostMapping("article")
+    public ResponseEntity<Void> addArticle(@RequestBody ArticleInfo articleInfo, UriComponentsBuilder builder) {
+        Article article = new Article();
+        BeanUtils.copyProperties(articleInfo, article);
+        boolean flag = articleService.addArticle(article);
+        if (flag == false) {
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/article/{id}").buildAndExpand(article.getArticleId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);*/
+    //@PostMapping("/getda")
+
    /* @PutMapping(path="/update")
     public List<Map<String, Object>> gdata()
     {
@@ -46,4 +71,4 @@ public class dvdrentalController {
     }
 */
 
-}
+    }
